798febac5f37031b739a8f0cdced5e772a1b270c
ahmad-azzam <ahmadazzam123654@gmail.com>
2021-09-17 14:25:21 +0700

    add Home Page

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import '../index.css'
+
+export default function Footer() {
+    return (
+        <>
+            <footer class="flex-none items-center p-4 footer bg-indigo-500 text-neutral-content">
+                <div class="flex items-center justify-center w-full">
+                    <p>Copyright © 2021 - EZ Laundr</p>
+                </div>
+            </footer>
+        </>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import '../index.css'
+
+export default function Navbar() {
+    return (
+        <>
+            <div className="flex-none h-16">
+                <div className="navbar mb-2 shadow-xl bg-indigo-500 text-neutral-content">
+                    <div className="flex-none hidden lg:flex">
+                        <button className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="flex-1 hidden px-2 mx-2 lg:flex">
+
+                    </div>
+                    <div className="flex-none">
+                        <button className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="flex-none">
+                        <div className="avatar">
+                            <div className="rounded-full w-10 h-10 m-1">
+                                <img src="https://i.pravatar.cc/500?img=32" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
