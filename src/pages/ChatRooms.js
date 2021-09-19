dfe78027e9df0e9fe497246c45f6c80932ec7358
ahmad-azzam <ahmadazzam123654@gmail.com>
2021-09-19 16:54:34 +0700

    add order

---
+++ src/pages/ChatRooms.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat'
+import {
+    Chat, Channel, Window, ChannelHeader, MessageList, MessageInput, Thread
+} from 'stream-chat-react'
+
+
+
+export default function ChatRooms() {
+    const idUser = 1
+    const client = StreamChat.getInstance('4zhahvr4har5', 'qh2vyhx4ruu3sgkv3wws6v72jz82swbkjzxxurjwq7yd49r8xgbsxh2gqk74nkav')
+    client.connectUser({
+        user_id: idUser
+    }, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.ZDF13b_LjvN0y9bYP7EkQkg1D1PmRpUwtRtXr5ORXyA')
+
+    const channel = client.channel('messaging', {
+        image: 'https://i.ibb.co/G9TWJY9/Batch-14-Ahmad-Azzam-S.png',
+        name: 'Testing Channel',
+        members: ['Customer 1', 'Customer 2'],
+    });
+
+    return (
+        <>
+            <Chat client={client}>
+                <Channel channel={channel}>
+                    <Window>
+                        <ChannelHeader />
+                        <MessageList />
+                        <MessageInput />
+                    </Window>
+                    <Thread />
+                </Channel>
+            </Chat>
+        </>
+    )
+}
