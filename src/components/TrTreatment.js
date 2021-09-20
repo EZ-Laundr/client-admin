0116fca0c58f77e01efc73dac9b40389191564e0
ariefkoko <arief.renaldy16@gmail.com>
2021-09-20 15:32:54 +0700

    add perfume price and fix special treatment name

--- src/components/TrTreatment.js
+++ src/components/TrTreatment.js
@@ -1,17 +1,15 @@
-import '../index.css'
+import "../index.css";
 
 export default function TrTreatment({ el, i }) {
-    return (
-        <>
-            <tr>
-                <th>{i + 1}</th>
-                <td>{el.name}</td>
-                <td>{el.price}</td>
-                <td>{el.quantity}</td>
-                <td>
-                    {Number(el.price) * Number(el.quantity)}
-                </td>
-            </tr>
-        </>
-    )
+	return (
+		<>
+			<tr>
+				<th>{i + 1}</th>
+				<td>{el.SpecialTreatment.name}</td>
+				<td>{el.price}</td>
+				<td>{el.quantity}</td>
+				<td>{Number(el.price) * Number(el.quantity)}</td>
+			</tr>
+		</>
+	);
 }
