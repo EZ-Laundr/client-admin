050c4cad4da2d68f990e106c4a1f00f762d09a49
ahmad-azzam <ahmadazzam123654@gmail.com>
2021-09-21 11:18:59 +0700

    handle conflict

--- src/components/TrTreatment.js
+++ src/components/TrTreatment.js
@@ -1,21 +1,6 @@
 import "../index.css";
 
 export default function TrTreatment({ el, i }) {
-<<<<<<< HEAD
-	return (
-		<>
-			<tr>
-				<th>{i + 1}</th>
-				<td>{el.SpecialTreatment.name}</td>
-				<td>{el.price}</td>
-				<td>{el.quantity}</td>
-				<td>{Number(el.price) * Number(el.quantity)}</td>
-			</tr>
-		</>
-	);
-}
-=======
-    console.log(el)
     return (
         <>
             <tr>
@@ -23,11 +8,10 @@ export default function TrTreatment({ el, i }) {
                 <td>{el.SpecialTreatment.name}</td>
                 <td>{el.price}</td>
                 <td>{el.quantity}</td>
-                <td>
-                    {Number(el.price) * Number(el.quantity)}
-                </td>
+                <td>{Number(el.price) * Number(el.quantity)}</td>
             </tr>
         </>
-    )
+    );
 }
->>>>>>> map
+
+
