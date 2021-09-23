eb2ee05b2ad7ef33a0d47518a3980bf721b00a8a
ahmad-azzam <ahmadazzam123654@gmail.com>
2021-09-23 19:58:44 +0700

    wow

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -59,55 +59,6 @@ export default function Detail({ changeLogin }) {
         }
     }
 
-    async function updateStatus() {
-        try {
-            const { isConfirmed } = await Swal.fire({
-                title: 'Are you sure?',
-                text: "You want to change order status ?!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, change it!'
-            })
-            if (isConfirmed) {
-                setLoadingDone(true)
-                const result = await orderApi({
-                    method: 'patch',
-                    url: `/${order.id}`
-                })
-                if (result) {
-                    setLoadingDone(false)
-                    fetchDetail()
-                }
-            }
-
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
-    useEffect(() => {
-        const changeTextAddress = JSON.parse(order.customerAddress)
-        const { latitude, longitude } = changeTextAddress
-        setLatCustomer(latitude)
-        setLongCustomer(longitude)
-    }, [])
-
-    useEffect(() => {
-        if (localStorage.getItem('access_token')) {
-            fetchDetail()
-        } else {
-            history.push('/login')
-        }
-    }, [])
-
-    useEffect(() => {
-        const before = order.totalPrice
-        const totalWeight = weight === 0 ? 1 : weight
-        setTotalPrice(before + Number(Number(priceService) * Number(totalWeight)))
-    }, [weight])
-
     async function processOrder() {
         try {
             const { isConfirmed } = await Swal.fire({
@@ -149,6 +100,57 @@ export default function Detail({ changeLogin }) {
             console.log(err)
         }
     }
+
+    // async function updateStatus() {
+    //     try {
+    //         const { isConfirmed } = await Swal.fire({
+    //             title: 'Are you sure?',
+    //             text: "You want to change order status ?!",
+    //             icon: 'warning',
+    //             showCancelButton: true,
+    //             confirmButtonColor: '#3085d6',
+    //             cancelButtonColor: '#d33',
+    //             confirmButtonText: 'Yes, change it!'
+    //         })
+    //         if (isConfirmed) {
+    //             setLoadingDone(true)
+    //             const result = await orderApi({
+    //                 method: 'patch',
+    //                 url: `/${order.id}`
+    //             })
+    //             if (result) {
+    //                 setLoadingDone(false)
+    //                 fetchDetail()
+    //             }
+    //         }
+
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+
+    useEffect(() => {
+        const changeTextAddress = JSON.parse(order.customerAddress)
+        const { latitude, longitude } = changeTextAddress
+        setLatCustomer(latitude)
+        setLongCustomer(longitude)
+    }, [])
+
+    useEffect(() => {
+        if (localStorage.getItem('access_token')) {
+            fetchDetail()
+        } else {
+            history.push('/login')
+        }
+    }, [])
+
+    useEffect(() => {
+        const before = order.totalPrice
+        const totalWeight = weight === 0 ? 1 : weight
+        setTotalPrice(before + Number(Number(priceService) * Number(totalWeight)))
+    }, [weight])
+
+
 
     return (
         <>
@@ -235,9 +237,9 @@ export default function Detail({ changeLogin }) {
                                                             {
                                                                 order.weight === 0 && <button style={{ backgroundColor: '#107CF1' }} onClick={processOrder} className={loading ? 'flex btn border-2 loading' : 'btn border-2 hover:shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'}>Process</button>
                                                             }
-                                                            {
+                                                            {/* {
                                                                 order.status === 'On Progress' && <button onClick={updateStatus} style={{ backgroundColor: '#48bb78' }} className={loadingDone ? 'btn border-2 loading' : "btn border-2 hover:shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"}>{!loadingDone && 'Done'}</button>
-                                                            }
+                                                            } */}
                                                             <button onClick={() => history.push('/')} className="btn border-2 bg-red-600  focus:outline-none hover:bg-red-600 nsition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Back</button>
                                                         </div>
                                                     </div>
